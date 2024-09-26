931b0d8fcc2189e7028d5d4f248f7c009fea6fe6
SpoilerRules <spoilerrulesowo@gmail.com>
2024-09-26 19:22:42 +0200

    clean up NavigationComponent.kt

--- src/main/kotlin/com/spoiligaming/explorer/ui/navigation/NavigationComponent.kt
+++ src/main/kotlin/com/spoiligaming/explorer/ui/navigation/NavigationComponent.kt
@@ -1,18 +1,14 @@
 package com.spoiligaming.explorer.ui.navigation
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import com.spoiligaming.explorer.ui.screens.home.FileBackupSubScreen
 import com.spoiligaming.explorer.ui.screens.home.HomeScreen
 import com.spoiligaming.explorer.ui.screens.settings.SettingsScreen
 
 @Composable
-fun NavigationComponent() {
-    val controller = remember { NavigationController }
-
-    when (controller.currentScreen) {
+fun NavigationComponent() =
+    when (NavigationController.currentScreen) {
         Screen.Home -> HomeScreen()
         Screen.Settings -> SettingsScreen()
         Screen.FileBackupScreen -> FileBackupSubScreen()
     }
-}
