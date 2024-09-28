5f443a50b2c64dd3680c52c2af5930514baf0460
SpoilerRules <spoilerrulesowo@gmail.com>
2024-09-28 15:48:03 +0200

    increase version

--- src/main/kotlin/com/spoiligaming/explorer/SoftwareInformation.kt
+++ src/main/kotlin/com/spoiligaming/explorer/SoftwareInformation.kt
@@ -1,6 +1,6 @@
 package com.spoiligaming.explorer
 
 object SoftwareInformation {
-    const val VERSION = "M11.1"
+    const val VERSION = "M11.2"
     const val DISCORD_SERVER_LINK = "https://discord.gg/fVA5Wr6Nns"
 }
