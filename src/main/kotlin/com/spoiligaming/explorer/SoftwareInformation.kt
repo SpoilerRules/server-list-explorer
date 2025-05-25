459546a5f8d4eb03ee6f87c183a22e856dc20d59
SpoilerRules <spoilerrulesowo@gmail.com>
2025-05-25 20:01:14 +0200

    update SoftwareInformation.kt

--- src/main/kotlin/com/spoiligaming/explorer/SoftwareInformation.kt
+++ src/main/kotlin/com/spoiligaming/explorer/SoftwareInformation.kt
@@ -1,6 +1,6 @@
 package com.spoiligaming.explorer
 
 object SoftwareInformation {
-    const val VERSION = "M12.1"
+    const val VERSION = "M12.2"
     const val DISCORD_SERVER_LINK = "https://discord.gg/fVA5Wr6Nns"
 }
