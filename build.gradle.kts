19918507fe407080e1cdc05d5cdf1f179aedb0cd
SpoilerRules <spoilerrulesowo@gmail.com>
2025-09-16 14:12:25 +0200

    build: upgrade JVM target from 17 to 21 (LTS)

--- build.gradle.kts
+++ build.gradle.kts
@@ -53,14 +53,14 @@ subprojects {
 
     plugins.withType<JavaBasePlugin> {
         extensions.configure<JavaPluginExtension> {
-            sourceCompatibility = JavaVersion.VERSION_17
-            targetCompatibility = JavaVersion.VERSION_17
+            sourceCompatibility = JavaVersion.VERSION_21
+            targetCompatibility = JavaVersion.VERSION_21
         }
     }
 
     tasks.withType<KotlinCompile>().configureEach {
         compilerOptions {
-            jvmTarget.set(JvmTarget.JVM_17)
+            jvmTarget.set(JvmTarget.JVM_21)
         }
     }
 
